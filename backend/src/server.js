1ace78c3b116528abf52dcd65eb75639d4ce505f
Mahesh Sale <maheshsale333@gmail.com>
2025-06-22 11:04:59 +0530

    Model Schema created

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,16 +5,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-console.log(process.env.MONGO_URI)
-
 const app = express()
+const PORT = process.env.PORT || 5001
 
 connectDB()
 
 app.use('/api/notes', notesRoutes)
 
 app.listen(5001, () => {
-      console.log('Server started on port 5001')
+      console.log('Server started on port', PORT)
 })
 
 
