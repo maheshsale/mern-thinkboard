162787b7f3af374201060efb1d0da965cf1d7297
Mahesh Sale <maheshsale333@gmail.com>
2025-06-22 10:48:08 +0530

    MongoDB configured

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes.js'
+import { connectDB } from './config/db.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+console.log(process.env.MONGO_URI)
 
 const app = express()
+
+connectDB()
 
 app.use('/api/notes', notesRoutes)
 
 app.listen(5001, () => {
       console.log('Server started on port 5001')
 })
+
+
 
